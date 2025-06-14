52621e1a3916a85e9da652fc22b46f9bdd5511f9
ztrainer <hozhangg@gmail.com>
2025-06-14 18:07:44 -0400

    Implement Tailwind CSS via CDN due to build issues

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {}, //Use the PostCSS plugin
-    autoprefixer: {},
-  },
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
+    import App from './App.jsx';
+    // import './index.css'; // This should be commented out or removed
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+    
